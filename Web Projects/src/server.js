f32979ec9876e694574523752320733a190c1d65
Léo <41534598+Syrql@users.noreply.github.com>
2022-06-01 21:55:56 +0200

    Add files via upload

---
+++ Web Projects/src/server.js
@@ -0,0 +1,31 @@
+/**
+ * 01/06/2022
+ * First learning lesson of Node.js
+ */
+
+let http = require('http');
+let fs = require('fs');
+let url = require("url");
+
+
+let server = http.createServer();
+
+server.on('request', (request, response) => {
+    response.writeHead(200);
+    let query = url.parse(request.url, true).query
+    let name = query.name === undefined ? 'anonyme' : query.name;
+
+    fs.readFile('index.html', 'utf-8', (err, data) => {
+        if (err) {
+            response.writeHead(404);
+            response.end("Ce fichier n'existe pas.")
+        } else {
+            response.writeHead(200, {
+                'Content-type': 'text/html; charset=utf-8'
+            });
+            data = data.replace('{{ name }}', name);
+            response.end(data);
+        }
+    });
+});
+server.listen(8080);
